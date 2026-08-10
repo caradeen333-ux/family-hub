334df6de835f612b455b5eb4f384fe752f8b027e
Mike <lansdownhigh@gmail.com>
2026-08-10 02:27:01 -0400

    Remove email addresses from public config

--- js/config.js
+++ js/config.js
@@ -15,9 +15,9 @@ const CONFIG = {
   // color: the accent color for this person in combined view
   // default: this person is shown in "My Day" by default on this device
   people: [
-    { name: 'Mike',    calendarId: 'primary',                          color: '#7c5cfc', default: true },
-    { name: 'Charlie', calendarId: 'charles.amadeus.clark@gmail.com',  color: '#5cc9fc', default: false },
-    { name: 'Avery',   calendarId: 'agranney@gmail.com',               color: '#fcd45c', default: false },
+    { name: 'Mike',    calendarId: 'primary', color: '#7c5cfc', default: true },
+    { name: 'Charlie', calendarId: '',        color: '#5cc9fc', default: false },
+    { name: 'Avery',   calendarId: '',        color: '#fcd45c', default: false },
   ],
 
   // Refresh interval in milliseconds (5 min)
